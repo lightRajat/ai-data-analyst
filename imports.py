e6b87c9e9c8aaba1d99dc4ea4b0154943cdccbad
Rajat <light.rajat+gh@gmail.com>
2026-02-10 12:37:05 +0530

    refactor: Shifted all imports to `main.py`

--- imports.py
+++
@@ -1,7 +0,0 @@
-from fastapi import FastAPI, File, UploadFile, Request
-from fastapi.middleware.cors import CORSMiddleware
-from typing import List
-import asyncio
-import time
-import random
-import utils
--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-from imports import *
+from fastapi import FastAPI, File, UploadFile, Request
+from fastapi.middleware.cors import CORSMiddleware
+from typing import List
+import asyncio
+import time
+import random
+import utils
 
 app = FastAPI()
 app.add_middleware(CORSMiddleware, allow_origins=["*"], allow_credentials=True, allow_methods=["*"], allow_headers=["*"])
